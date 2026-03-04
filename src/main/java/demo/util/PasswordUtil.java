8ef229cb6a90b82eebd301849e9599320b8a84a7
ravindra-agnihotri <99528805+ravindra-agnihotri@users.noreply.github.com>
2026-03-05 00:37:39 +0530

    Add password validation method to PasswordUtil

--- src/main/java/demo/util/PasswordUtil.java
+++ src/main/java/demo/util/PasswordUtil.java
@@ -1,4 +1,13 @@
 package demo.util;
 
 public class PasswordUtil {
+
+    public static boolean validatePassword(String password) {
+
+        if(password == null)
+            return false;
+
+        return password.length() > 5;
+    }
+
 }
